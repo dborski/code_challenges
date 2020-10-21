c3083ae696af91c0120764177db3ca23ef3e4928
Derek Borski <derek.borski@gmail.com>
2020-10-21 11:46:57 -0600

    Add directions code challenge

---
+++ directions.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+def dirReduc(arr, arr_length = 0)
+  opposites = {
+    "NORTH": "SOUTH",
+    "EAST": "WEST"
+  }
+  arr_length = arr.length
+  
+  holder = ''
+  arr.each_with_index do |direction, index|
+    if index > 0 && (opposites[holder.to_sym] == direction || opposites.key(holder) == direction.to_sym)
+      arr.slice!(index - 1, 2)
+      holder = ''
+    else
+      holder = direction
+    end 
+  end
+  dirReduc(arr, arr.length) if arr.length < arr_length
+  return arr
+end
+
+
+
+a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+u = ["NORTH", "WEST", "SOUTH", "EAST"]
+
+p dirReduc(a)
+p dirReduc(u)
