212c8d34c960a5056efc9d51485d0f8d68294f6a
Derek Borski <derek.borski@gmail.com>
2020-10-21 12:45:51 -0600

    Refactor solution just to remove pseudocode

--- persistence/persistence.rb
+++ persistence/persistence.rb
@@ -1,8 +1,4 @@
-require 'pry'
-
 def persistence(n, counter = 0)
-  # multiply all digits in n together, and then save the answer in a variable
-  # with a recursive solution, we will then keep multiplying the numbers until 
   if n < 10
     return counter
   else
