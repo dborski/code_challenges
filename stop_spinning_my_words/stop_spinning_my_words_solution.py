db7533a0ef8beb48e9fb24d76702a98d667c3263
Derek Borski <derek.borski@gmail.com>
2020-11-14 16:22:38 -0700

    Add stop_spinning_my_words problem and solution

---
+++ stop_spinning_my_words/stop_spinning_my_words_solution.py
@@ -0,0 +1,20 @@
+def spin_words(sentence):
+    split = sentence.split(' ')
+    for i, word in enumerate(split):
+      if len(word) > 4:
+        split[i] = word[::-1]
+
+    return " ".join(split)
+
+
+
+print(spin_words('Welcome'))
+# returns emocleW
+print(spin_words('Hey fellow warriors'))
+# returns Hey wollef sroirraw
+print(spin_words('This is a test'))
+# returns 'This is a test'
+print(spin_words('This is another test'))
+# returns 'This is rehtona test'
+
+
